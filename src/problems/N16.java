c116b2b5f4f05115f74bef69f9fd9dcf8532776c
jinhwan Bea <paewhal20@gmail.com>
2023-09-05 01:41:47 +0900

    구름 챌린지 16번째 연합

--- src/problems/N16.java
+++ src/problems/N16.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class N16 {
 
-    static boolean union = false;
+    static boolean union = false; // 연합 결성 여부
 
     public static void main(String[] args) throws IOException {
 
@@ -16,6 +16,7 @@ public class N16 {
         int N = Integer.parseInt(st.nextToken()); // 섬의 개수
         int M = Integer.parseInt(st.nextToken()); // 다리의 개수
 
+        // 인접 리스트
         List<Integer>[] list = new ArrayList[N + 1];
         for(int i = 1; i <= N; i++){
             list[i] = new ArrayList<>();
@@ -30,22 +31,23 @@ public class N16 {
             list[s].add(e);
         }
 
-        int count = 0;
-        boolean[] check = new boolean[N + 1];
-        Set<Integer> set = new HashSet<>();
+        int count = 0; // 연합의 갯수
+        boolean[] check = new boolean[N + 1]; // 연합이 결성된 노드 체크
+        Set<Integer> set = new HashSet<>(); // 연합 노드의 수
 
         for(int i = 1; i <= N; i++){
             if(!check[i]) {
-                DFS(i, check, set, list);
+                DFS(i, check, set, list); // 연합이 되있는 노드가 아닐 시 탐색 시작
             }
 
             if(union){
-                count++;
+                count++; // 연합일 시 연합 수(count) 증가
             }
 
-            union = false;
+            union = false; // 연합 boolean 초기화
         }
 
+        // 연합 수 = 전체 노드 - 연합 노드 수 + 연합 수
         int result = N - set.toArray().length + count;
 
         bw.write(String.valueOf(result));
@@ -54,17 +56,21 @@ public class N16 {
         br.close();
     }
 
+    /**
+     * 재귀 함수 이용
+     * 두 개의 노드가 연합 즉 서로 양방향 연결이라면 해당 연합 노드와 연결된 노드를 모두 찾아 true 처리한다.
+     */
     private static void DFS(int i, boolean[] check, Set<Integer> set, List<Integer>[] list) {
         for(int num : list[i]){
-            if(!check[num]) {
+            if(!check[num]) { // 해당 노드가 이미 연합 노드인지 확인
                 for(int value : list[num]){
-                    if(value == i){
+                    if(value == i){ // 서로 양방향 연결인지 확인
                         check[num] = true;
                         check[value] = true;
-                        union = true;
+                        union = true; // 연합 결성
                         set.add(num);
                         set.add(value);
-                        DFS(num, check, set, list);
+                        DFS(num, check, set, list); // 연합 결성된 노드와 연결된 노드 찾기
                     }
                 }
             }
